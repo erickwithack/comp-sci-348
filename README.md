190aaf78c507514d6df7b9add037f6ec48fe313b
Yael Ben Shalom <yael.bshalom@gmail.com>
2021-06-02 18:52:33 -0500

    add lab 7

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # CS348-IntroToAI
 
-A repository for CS-348-Intro to AI (2021) homework.
+A repository for CS348-Intro to AI (2021) homework.
 
 
 - **Lab0** - Practiced simple sorting algorithm in python.
 
-- **Lab1** - Practiced uninformed search methods (DF and BF search).
+- **Lab1** - Implement uninformed search methods (DF and BF search).
 
-- **Lab2** - Created a greedy hill climbing search algorithm to solve the 10-queens problem.
+- **Lab2** - Implement a greedy hill climbing search algorithm to solve the 10-queens problem.
 
-- **Lab3** - Created an A* search algorithm to find the way through a maze.
+- **Lab3** - Implement an A* search algorithm to find the way through a maze.
 
-- **Lab4** - Created a TIC-TAC-TOE solver capable to predict the result of a specific game when a board is provided, using min-max algorithm.
+- **Lab4** - Implement a TIC-TAC-TOE solver capable to predict the result of a specific game when a board is provided, using min-max algorithm.
 
-- **Lab5** - Created a sudoku solver capable using constraint satisfaction algorithm.
+- **Lab5** - Implement a sudoku solver capable using constraint satisfaction algorithm.
+
+- **Lab6** - Implement a path planning algorithm.
+
+- **Lab7** - Implement a set of shape detection algorithms.
+
+- **Lab8** - Implement classification algorithm using perceptron.
--- lab7/src/student_code.py
+++ lab7/src/student_code.py
@@ -1,23 +1,76 @@
 import common
-import math #note, for this lab only, your are allowed to import math
+import math  # note, for this lab only, your are allowed to import math
+
+WIDTH = common.constants.WIDTH
+HEIGHT = common.constants.HEIGHT
+CONVERTED_SIZE = 2000
+
 
 def detect_slope_intercept(image):
-	# PUT YOUR CODE HERE
-	# access the image using "image[y][x]"
-	# where 0 <= y < common.constants.WIDTH and 0 <= x < common.constants.HEIGHT 
-	# set line.m and line.b
-	# to create an auxiliar bidimentional structure 
-	# you can use "space=common.init_space(heigh, width)"
-	line=common.Line()
-	line.m=0
-	line.b=0
-	return line
+    space = common.init_space(CONVERTED_SIZE, CONVERTED_SIZE)
+    line = common.Line()
+
+    point_list = get_points(image)
+
+    for row in range(CONVERTED_SIZE):
+        for point in point_list:
+            # Change m range from [0, 2000] to [-10, 10]
+            m = row / 100 - 10
+            b = point[1] - m * point[0]  # y = mx + b  =>  b = y - mx
+
+            # Change b range from [-1000, 1000] to [0, 2000]
+            b_cast = int(b + 1000)
+
+            # Change m range from [-10, 10] to [0, 2000]
+            m_cast = int((m + 10) * 100)
+
+            if 0 <= b_cast < CONVERTED_SIZE and 0 <= m_cast < CONVERTED_SIZE:
+                space[b_cast][m_cast] += 1
+
+    max_value = -1
+    for b in range(CONVERTED_SIZE):
+        for m in range(CONVERTED_SIZE):
+            if space[b][m] > max_value:
+                max_value = space[b][m]
+                line.b = b - 1000
+                line.m = m / 100 - 10
+
+    return line
+
 
 def detect_circles(image):
-	# PUT YOUR CODE HERE
-	# access the image using "image[y][x]"
-	# where 0 <= y < common.constants.WIDTH and 0 <= x < common.constants.HEIGHT 
-	# to create an auxiliar bidimentional structure 
-	# you can use "space=common.init_space(heigh, width)"
-	return 0
-				
+    space = common.init_space(HEIGHT, WIDTH)
+    circle = common.Line()
+    circle.r = 30
+    circles_num = 0
+
+    point_list = get_points(image)
+
+    tot_steps = 2 * 360
+    angle_per_step = 2 * math.pi / tot_steps
+
+    for theta in range(tot_steps):
+        for point in point_list:
+            # (x - a)^2 + (y - b)^2 = r^2
+            a = int(point[0] + circle.r * math.cos(angle_per_step * theta) + 1)
+            b = int(point[1] - circle.r * math.sin(angle_per_step * theta) + 1)
+
+            if 0 <= a < HEIGHT and 0 <= b < WIDTH:
+                space[a][b] += 1
+
+    for a in range(HEIGHT):
+        for b in range(WIDTH):
+            if space[a][b] > 650:
+                circles_num += 1
+
+    return circles_num
+
+
+def get_points(image):
+    point_list = []
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            if image[y][x] == 0:
+                point_list.append((x, y))
+                
+    return point_list
